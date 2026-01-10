a45fd346032977beab6286dd3db0ecb126a8973c
amritseth <amritseth1210@gmail.com>
2026-01-10 20:21:15 +0530

    Change home route message in Flask app
    Updated the home route message to include the name 'Amrit Seth'.

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello! My Dockerized Flask App is running with CI/CD!"
+    return "Hello! My Name is Amrit Seth. My Dockerized Flask App is running with CI/CD!"
 
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=5000)
